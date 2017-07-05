9e9d715fd921a3504af544757e17e78c8be6b3a2
Linda Call <lindacall4home@hotmail.com>
2017-07-05 13:45:12 -0600

    wrote tests

--- test/sum-test.js
+++ test/sum-test.js
@@ -14,5 +14,31 @@ describe ('Sum', function() {
       expect(sum()).to.equal(0);
       assert.equal(sum(),0);
     });
-    
+
+    it('Should return undefined if non-numbers are passed in.', function(){
+      expect (sum('a')).to.be.an('undefined');
+      expect(sum('$')).to.be.an('undefined');
+      expect(sum([1,2])).to.be.an('undefined');
+      expect(sum({})).to.be.an('undefined');
+    });
+
+    it('Should return the sum of multiple numbers.', function(){
+      expect (sum(1, 2, 3)).to.equal(6);
+      expect(sum(7, 8, 9, 10)).to.equal(34);
+      expect(sum(0)).to.equal(0);
+      expect(sum(1)).to.equal(1);
+    });
+
+
+    it('Should allow negative numbers as arguments.', function(){
+      expect (sum(1, -1)).to.equal(0);
+      expect (sum(-1, -2)).to.equal(-3);
+    });
+
+    it('Should allow decimal numbers as arguments.', function(){
+      expect (sum(1.1, 2)).to.equal(3.1);
+      expect (sum(-1.1, -2.1)).to.equal(-3.2);
+    });
+
+
 });
